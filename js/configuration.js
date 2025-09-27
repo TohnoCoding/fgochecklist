e08d5f99f0af28078311e762596871f9a665d963
TohnoCoding <98242280+TohnoCoding@users.noreply.github.com>
2025-09-27 00:31:16 -0400

    Added Kawakami Gensai, Tōdō Heisuke & Harada Sanosuke

--- js/configuration.js
+++ js/configuration.js
@@ -220,7 +220,7 @@ const Config = {
     padoruEndMonth: 0, padoruEndDay: 3,         //  ...and Jan 3
     //#endregion
     
-    webAppVersion: "20250918"
+    webAppVersion: "20250927"
 };
 
 Config.cookieName = `${Config.webAppVersion}_update`;
