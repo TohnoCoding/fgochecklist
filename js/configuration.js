60ebb70af7f039f1cc5765b760ae67d826c4fe66
TohnoCoding <98242280+TohnoCoding@users.noreply.github.com>
2025-10-03 12:21:35 -0400

    Added Kondou Isami

--- js/configuration.js
+++ js/configuration.js
@@ -220,7 +220,7 @@ const Config = {
     padoruEndMonth: 0, padoruEndDay: 3,         //  ...and Jan 3
     //#endregion
     
-    webAppVersion: "20250927v3"
+    webAppVersion: "20251003"
 };
 
 Config.cookieName = `${Config.webAppVersion}_update`;
