3b9ec8dbf6786aed4810dcc8124ade32c942635f
TohnoCoding <98242280+TohnoCoding@users.noreply.github.com>
2025-10-11 19:14:09 -0400

    Fixed issue with Kondou Isami's internal ID

--- js/configuration.js
+++ js/configuration.js
@@ -220,7 +220,7 @@ const Config = {
     padoruEndMonth: 0, padoruEndDay: 3,         //  ...and Jan 3
     //#endregion
     
-    webAppVersion: "20251003"
+    webAppVersion: "20251011"
 };
 
 Config.cookieName = `${Config.webAppVersion}_update`;
