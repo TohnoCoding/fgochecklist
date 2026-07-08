e244eff4b789410f8bdb2f9361be874eedc21050
TohnoCoding <98242280+TohnoCoding@users.noreply.github.com>
2026-07-08 17:02:59 -0400

    Added Spacekigal and both Gray Lilies

--- js/configuration.js
+++ js/configuration.js
@@ -222,7 +222,7 @@ const Config = {
     
     lasagnablacklist:   // HACK: For whatever reason Lasengle decided to release
         ["5-54", "5-53", "5-52", "5-51", // Richard the Lionheart too early, so
-        "5-50", "5-49",           // when filtering for NA-only, unintended
+        "5-50", "5-49",                 // when filtering for NA-only, unintended
         "4-34", "4-35", "4-36", "4-37", "4-38", "4-39", // units were showing up.
         "4-40", "4-41"],
 
